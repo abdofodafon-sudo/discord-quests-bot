da6910bb422e46d52eb5efaa6532c4126cf6c635
abdofodafon-sudo <abdofodafon@gmail.com>
2026-08-22 17:15:08 +0300

    Modify config settings for developers and database
    Updated developer ID and changed database URL format.

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,19 +3,16 @@ import { config as dotenvConfig } from "dotenv";
 import { Config } from "../interface/config.js";
 dotenvConfig();
 
-
 export default {
     token: process.env.Token,
     embedColor: "#06c2fb",
     defaultLanguage: "en",
     debugMode: true,
-  //  allowedServers: ["1158846168957210635", "1399471603003428966"], // Example server IDs
+    // allowedServers: ["1158846168957210635", "1399471603003428966"],
     prefix: "!",
-    developers: ["527826654660132890"],
+    developers: ["916173342342533142"],
     database: {
         type: SupportedDatabaseTypes.MongoDB,
-        url: "mongodb://localhost:27017/badge2",
+        url: "mongodb+srv://Abdo_321_Abdo:XMaQFy3dNRs7cHFl@cluster0.i7lewj1.mongodb.net/badge2?retryWrites=true&w=majority",
     },
-
-
-} as Config
+} as Config;
